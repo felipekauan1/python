65e2e889a45c9fd5578bea59edec61243b211a7a
felipekauan1 <152967390+felipekauan1@users.noreply.github.com>
2024-08-17 18:35:47 -0300

    Update ex083.py

---
+++ ex/ex083.py
@@ -0,0 +1,17 @@
+expressao = str(input('Digite uma expressão númerica: '))
+pilha = []
+
+for caractere in expressao:
+    if caractere == '(':
+        pilha.append('(')
+    elif caractere == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+
+if len(pilha) == 0:
+    print('Sua expressão está valida!')
+else:
+    print('Sua expressão está errada!')
