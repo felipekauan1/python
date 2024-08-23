1fd71ce2ca473a9a058a2c9ab5d5929b5ad9c665
felipekauan1 <152967390+felipekauan1@users.noreply.github.com>
2024-08-23 19:13:55 -0300

    Create ex094.py

---
+++ ex/ex094.py
@@ -0,0 +1,52 @@
+rapaziada = []
+pessoa = {}
+soma_idade = 0
+
+while True:
+    pessoa['nome'] = str(input('Nome: '))
+
+    while True:
+        pessoa['sexo'] = str(input('Sexo: ')).upper()
+        if pessoa['sexo'] in 'FM':
+            break
+        else:
+            print('Digite um sexo valido!')
+    
+    pessoa['idade'] = int(input('Idade: '))
+
+    # adiciona os dados da pessoa na lista
+    rapaziada.append(pessoa.copy())
+    # limpa os dados da pessoa
+    pessoa.clear()
+
+    # se a resposta for sim, irá ler novos dados
+    resposta = str(input('Quer continuar? [S/N] '))
+    if resposta in 'Nn':
+        break
+
+print(f'O grupo tem {len(rapaziada)} pessoas.')
+
+for p in rapaziada:
+    soma_idade += p['idade']
+
+media = soma_idade / len(rapaziada)
+
+print(f'A média de idade é de {media:.2f} anos.')
+
+print(f'As mulheres cadastradas foram: ', end='')
+for p in rapaziada:
+    if p['sexo'] == 'F':
+        print(p['nome'], end=' ')
+print()
+
+print(f'Lista das pessoas que estão acima da média de idade: ')
+print()
+
+for p in rapaziada:
+    if p['idade'] > media:
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+        print()
+
+print('Programa encerrado!')
