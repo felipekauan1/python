4272317cb003e504c453fed41c0cc118f9b440ac
felipekauan1 <152967390+felipekauan1@users.noreply.github.com>
2024-08-23 16:45:09 -0300

    Create ex093.py

--- ex/ex092.py
+++ ex/ex092.py
@@ -1,9 +1,34 @@
+# importações
 from random import randint
 from datetime import date
 from time import sleep
 
-jogo = dict()
+# criando o dicionário dados
+dados = dict()
 
-jogo['idade'] = date.today().year - int(input('Digite o seu ano de nascimento: '))
+# lendo o nome
+dados['nome'] = str(input('Nome: '))
 
-print(jogo)
+# lendo o ano de nascimento
+ano_nascimento = int(input('Digite o seu ano de nascimento: '))
+# calculando a idade (data atual menos o ano de nascimento)
+dados['idade'] = date.today().year - ano_nascimento
+
+# lendo a carteira de trabalho(CTPS)
+dados['CTPS'] = int(input('Carteira de Trabalho (0 não tem): '))
+
+# se tiver carteira de trabalho irá ler itens adicionais
+if dados['CTPS'] != 0:
+    # lendo o ano de contratação
+    dados['ano de contratação'] = int(input('Ano de Contratação: '))
+
+    # lendo o salário
+    dados['salário'] = float(input('Salário: R$ '))
+
+    # calculando a idade que irá se aposentar
+    dados['aposentadoria'] = dados['ano de contratação'] - ano_nascimento + 35
+
+print('=-=' * 10)
+
+for chave, valor in dados.items():
+    print(f'{chave} tem o valor {valor}')
