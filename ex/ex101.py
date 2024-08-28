19e4709a6ed37cb67af119ff9bbaac771933f6c0
felipekauan1 <152967390+felipekauan1@users.noreply.github.com>
2024-08-28 15:30:45 -0300

    Create ex101.py

---
+++ ex/ex101.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+
+def voto(anonasc):
+    idade = date.today().year - anonasc
+
+    print(f'Com {idade} anos: ', end='')
+    if idade < 16:
+        print('Voto Negado.')
+    elif 16 <= idade < 18 or idade >= 65:
+        print('Voto Opcional.')
+    else:
+        print('Voto Obrigatório.')
+
+
+voto(int(input('Em que ano você nasceu? ')))
