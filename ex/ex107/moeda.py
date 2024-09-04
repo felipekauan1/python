c73521f4b1746c5f951bdc051b6bafc19b7d6f56
felipekauan1 <152967390+felipekauan1@users.noreply.github.com>
2024-09-04 13:37:30 -0300

    Create ex107

---
+++ ex/ex107/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(v, a):
+    v += (v * a / 100)
+    return v
+
+
+def diminuir(v, d):
+    v -= (v * d / 100)
+    return v
+
+
+def dobro(v):
+    return v * 2
+
+
+def metade(v):
+    return v / 2
